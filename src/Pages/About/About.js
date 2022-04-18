5de47befd733264e46b1713429cd082486cc1fa7
mdHasif2001 <iamhasib727@gmail.com>
2022-04-18 22:30:57 +0600

    about and blog section hasbeen created

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import me from '../../images/me/me1.png'
+import './About.css'
 
 const About = () => {
     return (
-        <div>
-            <h2>This about me</h2>
+        <div className='mt-3 container'>
+
+            <h2 className='text-center'>About Me</h2>
+
+            <div className='row container'>
+                <div className='col-lg-8 col-md-6 col-12 mt-5'>
+                    <p>Hi! I am Hasif. Right now I am learning web development with the help of programming hero team. Which is lead by Jhankar Mahbub. I already learn so many thing from this course. I already knew Html, css and some of css framework like bootstrap and tailwind. I also have some basic knowledge about JavaScript. Right know I am learning React js which a JavaScript library. Right know I am only focusing in this course. My target is to complete all the task step by step and finished the course perfectly. Wish me best luck!</p>
+                </div>
+
+                <div className='col-lg-4 col-md-6 col-12'>
+                    <img className='about-img' src={me} alt="" />
+                </div>
+
+
+            </div>
+
+
         </div>
     );
 };
--- src/Pages/Blog/BLog.js
+++ src/Pages/Blog/BLog.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import './Blog.css'
 
 const BLog = () => {
     return (
-        <div>
-            <h2>This is BLog</h2>
+        <div className='container'>
+            <h2 className='text-center mt-3'>BLog</h2>
+
+            <div className='blog-txt'>
+                <p><b>What is Difference between authorization and authentication?</b></p>
+                <p>Authentication হল কেউ কে তা যাচাই করার প্রক্রিয়া, যেখানে authorization  হল ব্যবহারকারীর কোন নির্দিষ্ট অ্যাপ্লিকেশন, ফাইল এবং ডেটাতে অ্যাক্সেস রয়েছে তা যাচাই করার প্রক্রিয়া।
+                    Authentication সাধারণত একটি ব্যবহারকারীর নাম এবং পাসওয়ার্ড দ্বারা করা হয়।
+                    অপরদিকে Authorization successfully authenticated হওয়ার পর বিভিন্ন জিনিস ব্যবহার করার পারমিশন দিয়ে থাকে যেমনঃ information, files, databases, funds, locations, almost anything.
+                </p>
+            </div>
+
+            <div className='blog-txt'>
+                <p><b>Why are you using firebase? What other options do you have to implement authentication?</b></p>
+                <p>Firebase Authentication সহজে ব্যবহারযোগ্য এবং এটি backend এ সার্ভিস দিয়ে থাকে।  এবং এটির SDK ব্যবহার করা অনেক সহজ। এবং এটি ready-made UI libraries দিয়ে থাকে। এটি পাসওয়ার্ড, ফোন নম্বর, Google, Facebook এবং Twitter এর মতো জনপ্রিয় authenticatio সার্ভিস provide করে থাকে। There are too many option that I can implement authentication like : Parse, Supabase, Kuzzle, Back4App etc
+
+                </p>
+            </div>
+
+            <div className='blog-txt'>
+                <p><b>What other services does firebase provide other than authentication?</b></p>
+                <p>Authentication ব্যতিত firebase আরও অনেক সার্ভিস প্রোভাইড করে থাকে যেমনঃ</p>
+                <li>Cloud Firestore
+                </li>
+                <li>Cloud Functions
+                </li>
+                <li>Authentication</li>
+                <li>Hosting</li>
+                <li>Cloud Storage
+                </li>
+                <li>Google Analytics
+                </li>
+                <li>Predictions</li>
+                <li>Cloud Messaging
+                </li>
+                <li>Dynamic Links
+                </li>
+                <li>Remote Config
+                </li>
+            </div>
+
         </div>
     );
 };
