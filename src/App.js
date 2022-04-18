5d8e87c7f731ba74459a8786dc8e4a62ca2e02dd
mdHasif2001 <iamhasib727@gmail.com>
2022-04-18 15:32:53 +0600

    checkout section with register hasbeen created

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Checkout from './Pages/Checkout/Checkout';
 import Home from './Pages/Home/Home/Home';
 import Login from './Pages/Login/Login/Login';
 import Register from './Pages/Login/Register/Register';
-// import RequireAuth from './Pages/Login/RequireAuth/RequireAuth';
+import RequireAuth from './Pages/Login/RequireAuth/RequireAuth';
 import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
@@ -20,16 +20,15 @@ function App() {
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/service/:serviceId' element={<ServiceDetail></ServiceDetail>}></Route>
 
-        {/* <RequireAuth>
+        <Route path="/checkout" element={
+          <RequireAuth>
             <Checkout></Checkout>
-          </RequireAuth> */}
+          </RequireAuth>
+        }></Route>
 
         <Route path='/about' element={<About></About>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
-
-
-
 
         <Route path='/blog' element={<BLog></BLog>}></Route>
         <Route path='/about' element={<About></About>}></Route>
--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -1,7 +1,5 @@
 import React from 'react';
 // import google from '../../../images/social/google.png'
-// import facebook from '../../../images/social/facebook.png'
-// import github from '../../../images/social/github.png'
 import { useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
 import { useNavigate } from 'react-router-dom';
@@ -50,22 +48,6 @@ const SocialLogin = () => {
                 className='btn btn-info w-50 d-block mx-auto my-2'>
                     <img style={{width: '30px'}} src='' alt="" />
                    <span className="px-2"> Google Sign IN</span></button>
-            </div>
-            
-
-            <div>
-                <button className='btn btn-info w-50 d-block mx-auto my-2'>
-                    <img style={{width: '30px'}} src='' alt="" />
-                   <span className="px-2"> Facebook Sign IN</span></button>
-            </div>
-
-
-            <div>
-                <button
-                    onClick={() => signInWithGithub()}
-                className='btn btn-info w-50 d-block mx-auto'>
-                    <img style={{width: '30px'}} src='' alt="" />
-                   <span className="px-2"> Github Sign IN</span></button>
             </div>
 
         </div>
