5210a8723e3743f2c1c393ff45b50a8675c8eb5a
mdHasif2001 <iamhasib727@gmail.com>
2022-04-18 08:59:49 +0600

    checkout section hasbeen touch

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
 import BLog from './Pages/Blog/BLog';
+import Checkout from './Pages/Checkout/Checkout';
 import Home from './Pages/Home/Home/Home';
 import Login from './Pages/Login/Login/Login';
+import RequireAuth from './Pages/Login/RequireAuth/RequireAuth';
 import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
@@ -16,6 +18,11 @@ function App() {
       <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/service/:serviceId' element={<ServiceDetail></ServiceDetail>}></Route>
+
+        <RequireAuth>
+            <Checkout></Checkout>
+          </RequireAuth>
+
 
         <Route path='/blog' element={<BLog></BLog>}></Route>
           <Route  path='/about' element={<About></About>}></Route>
